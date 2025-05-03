8a55ff50455bcbadab5d4cc0961353da024b6be8
= <=>
2025-05-03 20:26:37 +0300

    First commit. Added structure

---
+++ UserLibrary/UserLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserLibrary;
+
+namespace UserLibrary
+{
+    public class UsersLibrary
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string country { get; set; }
+        public int age { get; set; }
+
+    }
+
+
+
+}
+
+
